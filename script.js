0b28ee100e4697b3fa01831b08644912613fc85c
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-10-25 21:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function createTable() {
-    //Write your code here
+  const rowCount = parseInt(prompt("Input number of rows:"));
+  const colCount = parseInt(prompt("Input number of columns:"));
+
+  if (isNaN(rowCount) || isNaN(colCount)) {
+    alert("Please enter valid numbers for rows and columns.");
+    return;
+  }
+	const table = document.getElementById("myTable");
+
   
+  table.innerHTML = "";
+
+  
+  for (let i = 0; i < rowCount; i++) {
+    const row = table.insertRow(i);
+    for (let j = 0; j < colCount; j++) {
+      const cell = row.insertCell(j);
+      cell.textContent = `Row-${i} Column-${j}`;
+    }
+  }
 }
+
